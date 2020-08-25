c095c826370058e58eadc60b99f553e2090994ec
tehCh0nG <iamthechong@gmail.com>
2020-08-25 15:34:18 -0600

    Add files via upload
    Fix sprintf usage.

--- src/Exceptions/CouldNotSendNotification.php
+++ src/Exceptions/CouldNotSendNotification.php
@@ -4,8 +4,9 @@ namespace NotificationChannels\Signal\Exceptions;
 
 class CouldNotSendNotification extends \Exception
 {
-    public static function serviceRespondedWithAnError($response)
+    public static function serviceRespondedWithAnError(string $response)
     {
-        return new static("Unable to send message. Please ensure recipient is registered with Signal. Symfony output was: `{$response}['$symfonyerror']`");
+      $format = 'Unable to send message. Please ensure recipient is registered with Signal. Symfony output was: %s';
+      return new sprintf($format, $response);
     }
 }
